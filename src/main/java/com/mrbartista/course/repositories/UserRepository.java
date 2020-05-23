0fba6cd6da010186e3efbe82c8ee67b915146131
GeiBatista <geibatista@hotmail.com>
2020-05-22 23:04:13 -0300

    Service layer, component registration

--- src/main/java/com/mrbartista/course/repositories/UserRepository.java
+++ src/main/java/com/mrbartista/course/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.mrbartista.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.mrbartista.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
